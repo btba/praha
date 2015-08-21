2e1b93594afe29115ea35d4b52f0c2b30107b956
David B. Tucker <dbtleonia@gmail.com>
2015-08-21 00:05:52 -0400

    Make templates & static content edits take effect without re-running dev.sh.

--- dev.sh
+++ dev.sh
@@ -10,13 +10,11 @@ fi
 set -eux
 
 docker build -t mygorez gorez
-docker build -t myfiles nginx
 
-docker rm -f devnginx devfiles devgorez 2> /dev/null || true
+docker rm -f devnginx devgorez 2> /dev/null || true
 docker rmi $(docker images -f "dangling=true" -q) 2> /dev/null || true
 
-docker run -d -P -e "BOOKINGS_DSN=$1" --name devgorez mygorez
-docker run -d --name devfiles myfiles
-docker run -d -P --volumes-from devfiles --link devgorez:gorez --name devnginx nginx
+docker run -d -P -e "BOOKINGS_DSN=$1" -v $(pwd)/gorez/templates:/gorez/templates --name devgorez mygorez
+docker run -d -P -v $(pwd)/nginx/content:/usr/share/nginx/html -v $(pwd)/nginx/conf/nginx.conf:/etc/nginx/nginx.conf --link devgorez:gorez --name devnginx nginx
 
 echo http://$(boot2docker ip):$(docker inspect --format='{{index .NetworkSettings.Ports "80/tcp" 0 "HostPort"}}' devnginx)
