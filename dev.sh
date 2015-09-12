fb80a5e1dfa4fe8623d8ac62977907978bb6b745
David B. Tucker <dbtleonia@gmail.com>
2015-09-12 16:40:18 -0400

    Change from boot2docker to docker-machine.

--- dev.sh
+++ dev.sh
@@ -17,4 +17,4 @@ docker rmi $(docker images -f "dangling=true" -q) 2> /dev/null || true
 docker run -d -P -e "BOOKINGS_DSN=$1" -v $(pwd)/gorez/templates:/gorez/templates --name devgorez mygorez
 docker run -d -P -v $(pwd)/nginx/content:/usr/share/nginx/html -v $(pwd)/nginx/conf/nginx.conf:/etc/nginx/nginx.conf --link devgorez:gorez --name devnginx nginx
 
-echo http://$(boot2docker ip):$(docker inspect --format='{{index .NetworkSettings.Ports "80/tcp" 0 "HostPort"}}' devnginx)
+echo http://$(docker-machine ip default):$(docker inspect --format='{{index .NetworkSettings.Ports "80/tcp" 0 "HostPort"}}' devnginx)
--- prod_osx.sh
+++ prod_osx.sh
@@ -18,4 +18,4 @@ docker run -d -P -e "BOOKINGS_DSN=$1" --name prodgorez btba/praha-gorez
 docker run -d --name prodfiles btba/praha-nginx
 docker run -d -P --volumes-from prodfiles --link prodgorez:gorez --name prodnginx nginx
 
-echo http://$(boot2docker ip):$(docker inspect --format='{{index .NetworkSettings.Ports "80/tcp" 0 "HostPort"}}' prodnginx)
+echo http://$(docker-machine ip default):$(docker inspect --format='{{index .NetworkSettings.Ports "80/tcp" 0 "HostPort"}}' prodnginx)
