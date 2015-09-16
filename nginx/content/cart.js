e2b689bc15701adf476d8dfa6cdd1a95b1389a0f
David B. Tucker <dbtleonia@gmail.com>
2015-09-15 20:51:24 -0400

    Add price to cart.  Using single quotes in Javascript.

--- nginx/content/cart.js
+++ nginx/content/cart.js
@@ -1,11 +1,11 @@
 function updateItem(itemID) {
-    var quantity = parseInt(document.getElementById("items." + itemID + ".quantity").value)
+    var quantity = parseInt(document.getElementById('items.' + itemID + '.quantity').value)
     if (isNaN(quantity)) {
-	setAlert("Please enter a valid quantity");
+	setAlert('Please enter a valid quantity');
 	return;
     }
     if (quantity < 1 || quantity > 99) {
-	setAlert("Please enter a quantity between 1 and 99");
+	setAlert('Please enter a quantity between 1 and 99');
 	return;
     }
 
@@ -15,19 +15,19 @@ function updateItem(itemID) {
             if (xhr.status == 200) {
 		clearAlert();
 	    } else {
-		setAlert("Error updating item: " + xhr.responseText);
+		setAlert('Error updating item: ' + xhr.responseText);
             }
         }
     }
-    xhr.open("PUT", "/reservations/api/cartitems/" + itemID, true);
+    xhr.open('PUT', '/reservations/api/cartitems/' + itemID, true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send(JSON.stringify(quantity));
 }
 
 function removeItem(itemID) {
-    var itemRow = document.getElementById("items." + itemID);
+    var itemRow = document.getElementById('items.' + itemID);
     prevDisplay = itemRow.style.display;
-    itemRow.style.display = "none";
+    itemRow.style.display = 'none';
 
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
@@ -35,22 +35,22 @@ function removeItem(itemID) {
             if (xhr.status == 200) {
 		clearAlert()
 	    } else {
-                setAlert("Error deleting item: " + xhr.responseText);
+                setAlert('Error deleting item: ' + xhr.responseText);
 		itemRow.style.display = prevDisplay;
             }
         }
     }
-    xhr.open("DELETE", "/reservations/api/cartitems/" + itemID, true);
+    xhr.open('DELETE', '/reservations/api/cartitems/' + itemID, true);
     xhr.send();
 }
 
 function setAlert(s) {
-    var alert = document.getElementById("alert");
+    var alert = document.getElementById('alert');
     alert.innerHTML = s; 
-    alert.style.display = "block";
+    alert.style.display = 'block';
 }
 
 function clearAlert() {
-    var alert = document.getElementById("alert");
-    alert.style.display = "none";
+    var alert = document.getElementById('alert');
+    alert.style.display = 'none';
 }
